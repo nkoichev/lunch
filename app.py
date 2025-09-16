15216089db47b9291ea1aef7245dc480d3c7e9ab
nkoichev <nikolay.koychev@gmail.com>
2025-09-16 17:29:58 +0300

    ..

--- app.py
+++ app.py
@@ -98,9 +98,8 @@ with open("KjTHbioe5L.json", "r", errors='ignore') as f:
 
 col1, col2, col3 = st.columns([1.6, 7, 0.1])
 
-container = st.container(horizontal_alignment="distribute")
-with container:
-    st_lottie(data_lottie, loop=False, width=300, height=300)
+
+st_lottie(data_lottie, loop=False, width=300, height=300)
 
 check1 = st.toggle('Всички поръчали', value=True, key='check1')
 check2 = st.toggle('Обобщено по имена', value=True, key='check2')
